3aa84ba0e3c2b5f45fbf303f856a4e77d8e05a5b
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-03 10:21:27 +0330

    Add skeleton for dhcpNetworkListener

--- include/network/listener.h
+++ include/network/listener.h
@@ -1,6 +1,14 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #if !defined(NETWORK_LISTENER_H)
 #define NETWORK_LISTENER_H
 
-void listener (/* TODO parameters */);
+int dhcpNetworkListener (char *address, int port);
 
 #endif
--- src/listener.c
+++ src/listener.c
@@ -11,8 +11,61 @@
 
 #include "network/listener.h"
 
-void
-listener()
+int
+dhcpNetworkListener (char *address, int port)
 {
-  /* TODO */
+  /* TODO port validation */
+
+  /* TODO address validation */
+
+  /* init */
+  int dhcpSocket;
+
+  struct sockaddr_in dhcpServerAddress;
+
+  int retval;
+
+  dhcpSocket = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+  if (dhcpSocket == -1)
+    return EXIT_FAILURE;
+
+  dhcpServerAddress.sin_family = AF_INET;
+
+  dhcpServerAddress.sin_port = htons (port);
+
+  dhcpServerAddress.sin_addr.s_addr = inet_addr (address);
+
+  retval = bind (dhcpSocket, (struct sockaddr *)&dhcpServerAddress,
+                 sizeof (struct sockaddr));
+
+  if (retval == -1)
+    return EXIT_FAILURE;
+
+  while (1)
+    {
+      struct sockaddr_in dhcpClientAddress;
+
+      socklen_t dhcpClientAddressLen;
+
+      int fdReturnedValue = 0;
+
+      char packet[1024];
+
+      /* After accepting and connecting */
+
+      /* `TODO Recive Discovery */
+
+      /* `TODO Check requested ip address with ping */
+
+      // suitableIpAddressForLease = callback (discovery);
+
+      /* `TODO send offer with `suitableIpAddressForLease` informations */
+
+      /* `TODO recive request and compare with discover */
+
+      /* `TODO if everything is OK, send ack */
+
+      /* `TODO checking arp for dhcp starvation preventation */
+    }
 }
