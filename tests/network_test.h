901e959c79351b4aa468600d98aba97fcff9770c
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-04 18:00:53 +0330

    Mode all test fake datas to listener.h

--- tests/network_test.h
+++ tests/network_test.h
@@ -3,6 +3,14 @@
 #if !defined(TESTS_NETWORK_H)
 #define TESTS_NETWORK_H
 
+#define TEST_FAKE_DATA_DHCP_NETWORK_YOUR_IP_ADDRESS            "192.168.133.117"
+
+#define TEST_FAKE_DATA_DHCP_NETWORK_SERVER_IDENTIFIER        "192.168.133.30"
+#define TEST_FAKE_DATA_DHCP_NETWORK_IP_ADDRESS_LEASE_TIME    600
+#define TEST_FAKE_DATA_DHCP_NETWORK_SUBNET_MASK              "255.255.255.0"
+#define TEST_FAKE_DATA_DHCP_NETWORK_ROUTER                   "192.168.1.1"
+#define TEST_FAKE_DATA_DHCP_NETWORK_DOMAIN_NAME              "alireza.zharfpouyan.net"
+
 int initSuiteNetwork();
 
 int cleanupSuiteNetwork();
